3d24d6482ec1c8d943a16b2179b606949d8600a9
teacherpan <3792535@qq.com>
2020-09-02 14:11:38 +0800

    done

--- src/test/java/club/codedemo/springjpatestinmemorydatabase/SpringJpaTestInMemoryDatabaseApplicationTests.java
+++ src/test/java/club/codedemo/springjpatestinmemorydatabase/SpringJpaTestInMemoryDatabaseApplicationTests.java
@@ -9,5 +9,4 @@ class SpringJpaTestInMemoryDatabaseApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
--- src/test/java/club/codedemo/springjpatestinmemorydatabase/respository/StudentRepositoryTest.java
+++ src/test/java/club/codedemo/springjpatestinmemorydatabase/respository/StudentRepositoryTest.java
@@ -10,9 +10,12 @@ class StudentRepositoryTest {
     @Autowired
     StudentRepository studentRepository;
 
+    /**
+     * 由于本机并没有安装mysql（即使安装了，也没有exampleDb数据库）
+     * 所以如果本方法被成功执行，则说明当前单元测试连接的为H2数据库
+     */
     @Test
     void findAll() {
         this.studentRepository.findAll();
     }
-
 }
